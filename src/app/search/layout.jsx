6104a76f9ef9e107027851748ba0d4ac56593ccc
Sudharsan Ravikumar <supersudh@gmail.com>
2023-08-18 14:54:00 +0530

    create search page for web and image

---
+++ src/app/search/layout.jsx
@@ -0,0 +1,13 @@
+import SearchHeader from '@/components/SearchHeader'
+import '../globals.css'
+
+export default function SearchLayout({ children }) {
+  return (
+    <div>
+      <SearchHeader />
+      {children}
+      {/* Footer */}
+
+    </div>
+  )
+}
